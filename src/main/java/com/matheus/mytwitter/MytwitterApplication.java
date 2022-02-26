a80b01aa4f0067f128f4907d92ca3a7c86c5adde
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2022-02-26 17:14:34 -0300

    add: java spring initializer

---
+++ src/main/java/com/matheus/mytwitter/MytwitterApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.mytwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/mytwitter/MytwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.mytwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
